5fee77fa7ff7ce21aee9599c4daf84982fc88359
Eugenio Sales Siqueira <eugenio.sales@outlook.com>
2021-08-15 22:28:51 -0300

    teste do co authored
    Co-authored-by: Mexazonic <brunnohenriq697@gmail.com>

---
+++ latlong.py
@@ -0,0 +1,21 @@
+from math import sin, cos, sqrt, atan2, radians
+
+# approximate radius of earth in km
+def calc_distance_lat_long(lat1, lon1, lat2, lon2):
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
+
+print("Result:", calc_distance_lat_long(3,4, 2, 4))
+print("Should be:", 278.546, "km")
